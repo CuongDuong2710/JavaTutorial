05facc2e0884fcb9aa5461f9868068b5e00c7c40
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-10-20 08:21:41 +0700

    declaring inteface class

---
+++ JavaCode/src/designPattern_lesson1/Living.java
@@ -0,0 +1,14 @@
+package designPattern_lesson1;
+
+public interface Living {
+	
+	public void setName(String newName);
+	public String getName();
+	
+	public void setHeight(double newHeight);
+	public double getHeight();
+	
+	public void setFavFood(String newFood);
+	public String getFavFood();
+
+}
---
+++ JavaCode/src/designPattern_lesson1/Monkey.java
@@ -0,0 +1,46 @@
+package designPattern_lesson1;
+
+/**
+ * This class Monkey implements from interface Living
+ * @author QUOC CUONG
+ *
+ */
+public class Monkey implements Living{
+
+	@Override
+	public void setName(String newName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setHeight(double newHeight) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setFavFood(String newFood) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getFavFood() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
